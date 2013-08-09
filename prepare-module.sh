931919221f1acc290e8a0a03e25cec235ababc58
Kuy Mainwaring <kuy@outbreak.co>
2013-08-09 11:10:24 -0700

    added upload step to prepare-module script

--- prepare-module.sh
+++ prepare-module.sh
@@ -3,3 +3,7 @@
 # from the IDE root directory, find all files, remove those on the specified blacklist and use cpio (rather than tar) to create a clean tar file to pipe through gzip.
 #   the resulting tar.gz can be uploaded to moduleverse or otherwise distributed.
 find . | grep -v -E "/\.|\./prepare-module\.sh|/benches|/examples|/test|/tests" | cpio -o --format ustar | gzip -8 > ../nitride.release.tar.gz
+
+echo "Uploading module..."
+curl -v --user logiblock -X PUT --upload-file ../nitride.release.tar.gz http://moduleverse.com/v1/modules
+echo ""
